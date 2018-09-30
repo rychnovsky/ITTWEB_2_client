6da6166d97fb8cd44f11bed545ce82b5301318cd
Vojtěch Rychnovský <rychnvoj@fel.cvut.cz>
2018-09-30 23:55:08 +0200

    Added page not found and login screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,35 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
     path: 'workout/:id',
     component: AppComponent,
+    data: { title: 'Workout detail' },
   },
   {
     path: 'workouts',
     component: AppComponent,
+    data: { title: 'Workout list' },
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { title: 'Workout app login' },
   },
   {
     path: '',
     redirectTo: '/workouts',
     pathMatch: 'full',
   },
-  { path: '**', component: AppComponent },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    data: { title: 'Workout app - page not found' },
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PageHeaderComponent } from './page-header/page-header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, PageHeaderComponent],
+  declarations: [
+    AppComponent,
+    PageHeaderComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
