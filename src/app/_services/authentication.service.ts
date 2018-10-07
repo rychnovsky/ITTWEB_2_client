a063418c4c44b5d54223ef8d698bc223ba041bca
Vojtěch Rychnovský <rychnvoj@fel.cvut.cz>
2018-10-08 00:02:21 +0200

    Connect login form to service

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -48,53 +48,23 @@ export class AuthenticationService {
     }
   }
 
-  public login(user: User) {
+  public login(user: User): Observable<User> {
     const url = `${this.apiBaseUrl}/login`;
-    this.http.post<User>(url, user).subscribe(
-      data => {
+    return this.http.post<User>(url, user).pipe(
+      map(data => {
         this.saveToken(data.token);
-        return true;
-      },
-      // Errors will call this callback instead:
-      (err: HttpErrorResponse) => {
-        if (err.error instanceof Error) {
-          // A client-side or network error occurred. Handle it accordingly.
-          console.log('An error occurred:', err.error.message);
-        } else {
-          // The backend returned an unsuccessful response code.
-          // The response body may contain clues as to what went wrong, console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
-          console.log(
-            `Backend returned code ${err.status}, body was: ${err.error}`,
-          );
-        }
-        return false;
-      },
+        return data;
+      }),
     );
   }
 
   public register(user: User): Observable<User> {
     const url = `${this.apiBaseUrl}/register`;
     return this.http.post<User>(url, user).pipe(
-      map(
-        data => {
-          this.saveToken(data.token);
-          return data;
-        },
-        // Errors will call this callback instead:
-        (err: HttpErrorResponse) => {
-          if (err.error instanceof Error) {
-            // A client-side or network error occurred. Handle it accordingly.
-            console.log('An error occurred:', err.error.message);
-          } else {
-            // The backend returned an unsuccessful response code.
-            // The response body may contain clues as to what went wrong, console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
-            console.log(
-              `Backend returned code ${err.status}, body was: ${err.error}`,
-            );
-          }
-          return false;
-        },
-      ),
+      map(data => {
+        this.saveToken(data.token);
+        return data;
+      }),
     );
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../_models/user.model';
+import { AuthenticationService } from '../_services/authentication.service';
+
+import { first } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +12,47 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  email: string;
+  password: string;
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+  ) {}
 
   onSubmit() {
-    console.log('login submited');
-    this.router.navigate(['/workouts']);
+    console.log('submited');
+
+    let user: User = new User();
+    user.email = this.email;
+    user.password = this.password;
+
+    console.log(user);
+
+    this.authService
+      .login(user)
+      .pipe(first())
+      .subscribe(
+        data => {
+          console.log(data);
+          debugger;
+          this.router.navigate(['/']);
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.log('An error occurred:', err.error.message);
+          } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong, console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+            console.log(
+              `Backend returned code ${err.status}, body was: ${err.error}`,
+            );
+          }
+          return false;
+        },
+      );
+
     return false;
   }
 }
